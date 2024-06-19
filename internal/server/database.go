94752d05e1a75e2ab9ac3ae21364f272370a023d
Nickel Mattera <me@nicholemattera.gay>
2024-06-18 20:20:11 -0500

    ✨ Add database connection

---
+++ internal/server/database.go
@@ -0,0 +1,69 @@
+package server
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var DatabaseKey = "db"
+
+func initAndMigrate(db *sql.DB) {
+	// Database Initialization
+	transaction, err := db.Begin()
+	if err != nil {
+		log.Fatal("Unable to begin transation for database initialization", err)
+		return
+	}
+
+	transaction.Exec(`CREATE TABLE IF NOT EXISTS events (
+		id int unsigned NOT NULL AUTO_INCREMENT,
+		name varchar(256) NOT NULL,
+		number_of_registrations int unsigned NOT NULL,
+		PRIMARY KEY (id)
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;`)
+
+	err = transaction.Commit()
+	if err != nil {
+		log.Fatal("Error commiting transaction for database initialization", err)
+		return
+	}
+}
+
+func Database() *sql.DB {
+	username, usernameExists := os.LookupEnv("OUTCLIMB_DB_USER")
+	if !usernameExists {
+		log.Fatal("Error: No database username provided")
+		return nil
+	}
+
+	password, passwordExists := os.LookupEnv("OUTCLIMB_DB_PASSWORD")
+	if !passwordExists {
+		log.Fatal("Error: No database password provided")
+		return nil
+	}
+
+	host, hostExists := os.LookupEnv("OUTCLIMB_DB_HOST")
+	if !hostExists {
+		log.Fatal("Error: No database hostname provided")
+		return nil
+	}
+
+	name, nameExists := os.LookupEnv("OUTCLIMB_DB_NAME")
+	if !nameExists {
+		log.Fatal("Error: No database name provided")
+		return nil
+	}
+
+	db, err := sql.Open("mysql", username+":"+password+"@("+host+")/"+name+"?parseTime=true")
+	if err != nil {
+		log.Fatal("Error: Unable to connect to MySQL server", err)
+		return nil
+	}
+
+	initAndMigrate(db)
+
+	return db
+}
--- internal/server/router.go
+++ internal/server/router.go
@@ -1,30 +1,39 @@
 package server
 
 import (
+	"database/sql"
 	"net/http"
 )
 
-func ping(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("pong\n"))
+func pingHandler() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("pong\n"))
+	})
 }
 
-func check(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "https://outclimb.gay/event-registration-form", http.StatusTemporaryRedirect)
+func checkHandler(db *sql.DB) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "https://outclimb.gay/event-registration-form", http.StatusTemporaryRedirect)
+	})
 }
 
-func registration(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"success":true}`))
+func registrationHandler(db *sql.DB) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"success":true}`))
+	})
 }
 
 func SetupRouter() {
+	db := Database()
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /ping", ping)
-	mux.HandleFunc("GET /check", check)
-	mux.HandleFunc("POST /registration", registration)
+	mux.HandleFunc("GET /ping", pingHandler())
+	mux.HandleFunc("GET /check", checkHandler(db))
+	mux.HandleFunc("POST /registration", registrationHandler(db))
 
 	http.ListenAndServe(":80", mux)
 }
