30cbc29a0284f5fea87af7c8b606a404ff7eeb43
Nickel Mattera <me@nicholemattera.gay>
2024-09-21 18:54:43 -0500

    feat: remove camp and climb routes

--- internal/server/check.go
+++ internal/server/check.go
@@ -17,8 +17,6 @@ func NewCheckHandler(mux *http.ServeMux, db *sql.DB) {
 	}
 
 	mux.HandleFunc("GET /check", checkHandler.Check())
-	mux.HandleFunc("GET /check/2024-camp-and-climb", checkHandler.CheckCampAndClimb())
-	mux.HandleFunc("GET /check/2024-camp", checkHandler.CheckCamp())
 }
 
 func (ch *CheckHandler) Check() http.HandlerFunc {
@@ -27,30 +25,6 @@ func (ch *CheckHandler) Check() http.HandlerFunc {
 
 		if latestEvent.NumberOfRegistrations < 12 {
 			http.Redirect(w, r, "https://outclimb.gay/event-registration-form", http.StatusTemporaryRedirect)
-		} else {
-			http.Redirect(w, r, "https://outclimb.gay/event-registration-filled", http.StatusTemporaryRedirect)
-		}
-	})
-}
-
-func (ch *CheckHandler) CheckCampAndClimb() http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		latestEvent, _ := model.GetEvent(ch.db, "2024-camp-and-climb")
-
-		if latestEvent.NumberOfRegistrations < 12 {
-			http.Redirect(w, r, "https://outclimb.gay/2024-camp-and-climb-registration-form", http.StatusTemporaryRedirect)
-		} else {
-			http.Redirect(w, r, "https://outclimb.gay/event-registration-filled", http.StatusTemporaryRedirect)
-		}
-	})
-}
-
-func (ch *CheckHandler) CheckCamp() http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		latestEvent, _ := model.GetEvent(ch.db, "2024-camp")
-
-		if latestEvent.NumberOfRegistrations < 36 {
-			http.Redirect(w, r, "https://outclimb.gay/2024-camp-registration-form", http.StatusTemporaryRedirect)
 		} else {
 			http.Redirect(w, r, "https://outclimb.gay/event-registration-filled", http.StatusTemporaryRedirect)
 		}
